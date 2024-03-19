b5368e0c6987bb02a3e23d17128f768df6f61ddb
RMYP <160878278+RMYP@users.noreply.github.com>
2024-03-19 23:54:37 +0800

    add mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,51 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
 const app = require("./app")
-const PORT = 8000;
+const PORT = process.env.PORT ;
+
+// pelajari promise
+const db = process.env.database;
+mongoose.connect(db, {
+useNewUrlParser: true,
+}).then(con => {
+    console.log("connection success");
+});
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Name must exist"]
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    phoneNum: {
+        type: Number,
+        require: true
+    },
+    city : String,
+    country: {
+        type: String,
+        require: true,
+        default: "Indonesia"
+    }
+});
+
+const Customer = mongoose.model('Customers', customerSchema);
+
+const customerTest = new Customer({
+    name: 'Anastasia',
+    email: 'anastasia12@gmail.com',
+    phoneNum: 77777777777,
+});
+
+customerTest.save().then(doc => {
+    console.log(doc)
+}).catch((err) => {
+    console.log("error" + err)
+});
 
 app.listen(PORT, () => {
-    console.log(`app running on port ${PORT}`)
+    console.log(`app running on port ${PORT}`);
 })
